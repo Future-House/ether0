600837dca0ec3cf94d456b4998f5e6d7c7c2b68d
James Braza <jamesbraza@gmail.com>
2025-06-11 12:48:23 -0700

    Refactored out evaluation.py to move open answer utilites there

--- packages/qa/src/ether0/zero_shot.py
+++ packages/qa/src/ether0/zero_shot.py
@@ -1,85 +1,25 @@
 import asyncio
 import pathlib
-from collections.abc import Iterable, Mapping, Sequence
-from typing import Any, Self
-from uuid import UUID
+from collections.abc import Iterable
 
 from aviary.core import Message
-from aviary.utils import (
-    EvalAnswerMode,
-    MultipleChoiceEvaluation,
-    MultipleChoiceQuestion,
-    eval_answer,
-)
+from aviary.utils import MultipleChoiceEvaluation
 from datasets import Dataset
 from lmi import LiteLLMModel
-from pydantic import Field
 from tqdm.asyncio import tqdm_asyncio
 
-ETHER0_QA_PACKAGE_DIR = pathlib.Path(__file__).parent
-REPO_QA_EVAL_JSONL_PATH = ETHER0_QA_PACKAGE_DIR / "repo_qa_eval.jsonl"
-REPO_QA_STEP0_JSONL_PATH = ETHER0_QA_PACKAGE_DIR / "repo_qa_s0.jsonl"
+from ether0.evaluation import (
+    ETHER0_QA_PACKAGE_DIR,
+    REPO_QA_EVAL_JSONL_PATH,
+    Ether0RepoOpenAnswer,
+)
+
 REPO_ROOT_DIR = ETHER0_QA_PACKAGE_DIR.parent.parent.parent.parent
 REPO_ROOT_FILES = [str(f) for f in REPO_ROOT_DIR.iterdir() if not f.is_dir()]
 README_PATH = REPO_ROOT_DIR / "README.md"
 ETHER0_PACKAGE_DIR = REPO_ROOT_DIR / "src" / "ether0"
 REWARDS_PATH = ETHER0_PACKAGE_DIR / "rewards.py"
 CHAT_PATH = ETHER0_PACKAGE_DIR / "chat.py"
-
-
-LLM_SCORE_EVAL_CONFIG: dict[str, Any] = {
-    "prompt": (
-        "Here is a question, the correct answer to the question,"
-        " and a proposed answer to the question."
-        " We are going to grade the proposed answer as"
-        " correct (1.0), incorrect (0.0), or unsure (-1.0)."
-        " If the proposed answer matches the correct answer,"
-        " respond with just 1.0."
-        " If the proposed answer does not match the correct answer,"
-        " respond with just 0.0."
-        " Otherwise if unsure about the match,"
-        " respond with -1.0."
-        "\n\nQuestion: {question}"
-        "\n\nCorrect answer: {correct_answer}"
-        "\n\nProposed answer: {proposed_answer}"
-    ),
-    "max_score": 1.0,
-    "model": "gpt-4o",
-    "temperature": 0.0,
-}
-
-
-class Ether0RepoOpenAnswer(MultipleChoiceQuestion):
-    prompt_without_id: bool = True
-    prompt_without_options: bool = True
-    options: Sequence[str] = Field(default_factory=list)
-    unsure_answer: str | None = None
-
-    @classmethod
-    def from_ds(cls, row: Mapping[str, str]) -> Self:
-        return cls(
-            question_id=UUID(row["id"]),
-            question=row["question"],
-            ideal_answer=row["answer"],
-        )
-
-    async def grade(
-        self, proposed_answer: str
-    ) -> tuple[MultipleChoiceEvaluation, str | None]:
-        evaluation = await eval_answer(
-            proposed=proposed_answer,
-            correct=self.ideal_answer,
-            question=self.question_prompt,
-            eval_mode=EvalAnswerMode.LLM_SCORE,
-            llm_eval_config=LLM_SCORE_EVAL_CONFIG,
-        )
-        if evaluation == 1:
-            return MultipleChoiceEvaluation.CORRECT, str(evaluation)
-        if evaluation == 0:
-            return MultipleChoiceEvaluation.INCORRECT, str(evaluation)
-        if evaluation == -1:
-            return MultipleChoiceEvaluation.UNSURE, str(evaluation)
-        raise NotImplementedError(f"Didn't handle {evaluation=}.")
 
 
 OPEN_ANSWER_PREFIX = (
