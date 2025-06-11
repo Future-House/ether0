32c8134c3d9d467f3cd87f4166530659442e28ef
James Braza <jamesbraza@gmail.com>
2025-06-11 11:48:12 -0700

    Refactored/renamed models.py into zero_shot.py and renamed repo_qa.jsonl to repo_qa_eval.jsonl, to be clear

--- packages/qa/src/ether0/zero_shot.py
+++ packages/qa/src/ether0/zero_shot.py
@@ -1,6 +1,6 @@
 import asyncio
-from collections.abc import Mapping, Sequence
-from pathlib import Path
+import pathlib
+from collections.abc import Iterable, Mapping, Sequence
 from typing import Any, Self
 from uuid import UUID
 
@@ -16,12 +16,12 @@ from lmi import LiteLLMModel
 from pydantic import Field
 from tqdm.asyncio import tqdm_asyncio
 
-ETHER0_QA_PACKAGE_DIR = Path(__file__).parent
-REPO_QA_JSONL_PATH = ETHER0_QA_PACKAGE_DIR / "repo_qa.jsonl"
-
+ETHER0_QA_PACKAGE_DIR = pathlib.Path(__file__).parent
+REPO_QA_EVAL_JSONL_PATH = ETHER0_QA_PACKAGE_DIR / "repo_qa_eval.jsonl"
+REPO_QA_STEP0_JSONL_PATH = ETHER0_QA_PACKAGE_DIR / "repo_qa_s0.jsonl"
 REPO_ROOT_DIR = ETHER0_QA_PACKAGE_DIR.parent.parent.parent.parent
+REPO_ROOT_FILES = [str(f) for f in REPO_ROOT_DIR.iterdir() if not f.is_dir()]
 README_PATH = REPO_ROOT_DIR / "README.md"
-
 ETHER0_PACKAGE_DIR = REPO_ROOT_DIR / "src" / "ether0"
 REWARDS_PATH = ETHER0_PACKAGE_DIR / "rewards.py"
 CHAT_PATH = ETHER0_PACKAGE_DIR / "chat.py"
@@ -49,7 +49,7 @@ LLM_SCORE_EVAL_CONFIG: dict[str, Any] = {
 }
 
 
-class Ether0OpenAnswer(MultipleChoiceQuestion):
+class Ether0RepoOpenAnswer(MultipleChoiceQuestion):
     prompt_without_id: bool = True
     prompt_without_options: bool = True
     options: Sequence[str] = Field(default_factory=list)
@@ -88,25 +88,25 @@ OPEN_ANSWER_PREFIX = (
 )
 
 
-def format_prompt(question: str, *paths: Path) -> str:
-    """Format a message with optional context files."""
+def format_zero_shot_prompt(question: str, *paths: pathlib.Path) -> str:
+    """Format a message with optional context files for zero shotting LLMs."""
     return "\n\n".join(
         [OPEN_ANSWER_PREFIX]
         + [
-            "For context, here is the"
-            f" {path.name}:\n\n{path.read_text(encoding='utf-8').strip()}"
+            f"For context, here is the {path.relative_to(REPO_ROOT_DIR)!s}:"
+            f"\n\n{path.read_text(encoding='utf-8').strip()}"
             for path in paths
         ]
         + [question]
     )
 
 
-async def run_grade_eval(
-    model: LiteLLMModel, qa: Ether0OpenAnswer, *paths: Path
+async def zero_shot_then_grade(
+    model: LiteLLMModel, qa: Ether0RepoOpenAnswer, *paths: pathlib.Path
 ) -> MultipleChoiceEvaluation:
     # NOTE: this code has many extra locals to enable convenient debugging
     question, correct_answer = qa.question_prompt, qa.ideal_answer  # noqa: F841
-    prompt = format_prompt(question, *paths)
+    prompt = format_zero_shot_prompt(question, *paths)
     llm_result = await model.acompletion([Message(content=prompt)])
     if len(llm_result) != 1 or not llm_result[0].text:
         raise NotImplementedError(f"Unexpected shape of LLM result {llm_result}.")
@@ -116,16 +116,14 @@ async def run_grade_eval(
     return evaluation
 
 
-async def run_eval(*paths: Path) -> tuple[float, float]:
+async def bulk_zero_shot_then_grade(
+    model: LiteLLMModel,
+    open_answers: Iterable[Ether0RepoOpenAnswer],
+    *paths: pathlib.Path,
+) -> tuple[float, float]:
     """Evaluate on the eval dataset and return a two-tuple of accuracy and precision."""
-    open_answers = [
-        Ether0OpenAnswer.from_ds(row)
-        for row in Dataset.from_json(str(REPO_QA_JSONL_PATH))
-    ]
-
-    model = LiteLLMModel(name="gpt-4o")
     evaluations = await tqdm_asyncio.gather(
-        *(run_grade_eval(model, oa, *paths) for oa in open_answers),
+        *(zero_shot_then_grade(model, oa, *paths) for oa in open_answers),
         desc="Running evaluation",
         ncols=0,
     )
@@ -133,6 +131,11 @@ async def run_eval(*paths: Path) -> tuple[float, float]:
 
 
 async def main() -> None:
+    model = LiteLLMModel(name="gpt-4o")
+    open_answers = [
+        Ether0RepoOpenAnswer.from_ds(row)
+        for row in Dataset.from_json(str(REPO_QA_EVAL_JSONL_PATH))
+    ]
     for world in (
         (),
         (README_PATH,),
@@ -140,10 +143,16 @@ async def main() -> None:
         (README_PATH, REWARDS_PATH),
         (README_PATH, REWARDS_PATH, CHAT_PATH),
     ):
-        accuracy, precision = await run_eval(*world)
-        world_name = " + ".join([w.name for w in world]) if world else "Null"
+        accuracy, precision = await bulk_zero_shot_then_grade(
+            model, open_answers, *world
+        )
+        world_name = (
+            " + ".join([str(w.relative_to(REPO_ROOT_DIR)) for w in world])
+            if world
+            else "Null"
+        )
         print(
-            f"{world_name} world..."
+            f"{world_name!r} world..."
             f" accuracy: {accuracy:.2%}, precision: {precision:.2%}."
         )
 
